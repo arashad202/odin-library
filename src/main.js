8a2a588269c9c9421e5702b7aa06950be175b524
arashad202 <ahm.rashad.202@gmail.com>
2025-06-21 06:16:02 +0300

    Card view
    Write a function that loops through the array
    and displays each book on the page in a card.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
+// initialize library
 const myLibrary = [];
 
-
+// constructor function to create books
 function Book (title, author, pages, read) {
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
@@ -19,11 +20,29 @@ function Book (title, author, pages, read) {
 // let theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, false);
 // console.log(theHobbit.info());
 
+// function to add book to library
 function addBookToLibrary (title, author, pages, read) {
     let myBook = new Book(title, author, pages, read);
     myLibrary.push(myBook);
 }
 
-// addBookToLibrary("The Hobbit", "J.R.R Tolkien", 295, false);
-// console.log(myLibrary);
+addBookToLibrary("The Hobbit", "J.R.R Tolkien", 295, false);
 
+// access the booklist view
+const myBookList = document.querySelector('#bookList');
+
+// for everybook in view return a card for the book and store all cards in an array and append the new card to the booklistview
+const bookCards = myLibrary.map(book => {
+    const myCard = `<div class="w-[300px] h-[300px] border border-black rounded-3xl px-5 py-5">
+        <ul>
+          <li>ID: ${book.id}</li>
+          <li>Title: ${book.title}</li>
+          <li>Author: ${book.author}</li>
+          <li>Pages: ${book.pages}</li>
+          <li>read: ${book.read}</li>
+        </ul>
+      </div>`;
+    return myCard;
+});
+
+myBookList.innerHTML = bookCards.join(" ");
